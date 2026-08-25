7453d6b2590ca484732e36a7ab1c6a39a93cbdea
Karan tolambiya <karan.tolambiya@clouddrove.com>
2026-08-25 22:17:17 +0530

    ci: updated tf check to smurf tf check (#17)
    * fix: updated tf check to smurf tf check
    
    * fix: added time and azuread provider

--- versions.tf
+++ versions.tf
@@ -6,5 +6,15 @@ terraform {
       source  = "hashicorp/azurerm"
       version = ">= 4.0"
     }
+
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = ">= 3.0"
+    }
+
+    time = {
+      source  = "hashicorp/time"
+      version = ">= 0.9"
+    }
   }
 }
